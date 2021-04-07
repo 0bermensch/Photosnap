615ddd343a9efa07735de3fd869348b7a95a1c3d
Jason <jason.lo.kc@gmail.com>
2021-04-06 22:21:44 -0700

    fixed some bugs

--- src/components/pricing/PricingPrices.js
+++ src/components/pricing/PricingPrices.js
@@ -19,26 +19,26 @@ const PricingPrices = () => {
           />
         </button>
         <span>Annually</span>
-        <div className="pricing__section__cards__container">
-          <PricingCard
-            plan="Basic"
-            description="Includes basic usage of our platform. Recommended for new and aspiring photographers."
-            price={payment ? "$19.00" : "$190.00"}
-            billingCycle={payment}
-          />
-          <PricingCard
-            plan="Pro"
-            description="More advanced features available. Recommended for photography veterans and professionals."
-            price={payment ? "$39.00" : "$390.00"}
-            billingCycle={payment}
-          />
-          <PricingCard
-            plan="Business"
-            description="Additional features available such as more detailed metrics. Recommended for business owners."
-            price={payment ? "$99.00" : "$990.00"}
-            billingCycle={payment}
-          />
-        </div>
+      </div>
+      <div className="pricing__section__cards__container">
+        <PricingCard
+          plan="Basic"
+          description="Includes basic usage of our platform. Recommended for new and aspiring photographers."
+          price={payment ? "$19.00" : "$190.00"}
+          billingCycle={payment}
+        />
+        <PricingCard
+          plan="Pro"
+          description="More advanced features available. Recommended for photography veterans and professionals."
+          price={payment ? "$39.00" : "$390.00"}
+          billingCycle={payment}
+        />
+        <PricingCard
+          plan="Business"
+          description="Additional features available such as more detailed metrics. Recommended for business owners."
+          price={payment ? "$99.00" : "$990.00"}
+          billingCycle={payment}
+        />
       </div>
     </div>
   );
